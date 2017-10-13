8990443a8e65f7d0c8896904f2f05e900601070c
Johnny Lee-Othon <jleeothon@icloud.com>
2017-10-13 11:07:25 +0200

    Edited main.js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+const {VowelChart} = require('VowelChart.js')
+
 function draw () {
   var canvas = document.getElementById('vowel-chart')
   var ctx = canvas.getContext('2d')
-  var chart = new VowelChart(canvas)
+  var chart = new VowelChart(canvas, ctx)
   canvas.height = 2 * chart.padding + chart.height
   chart.drawBorder()
+}
+
+module.exports = {
+  draw,
 }
