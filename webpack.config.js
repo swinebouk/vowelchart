55a94fa9662484a603ef31c6b13b1b06c4611de4
Johnny Lee-Othon <jleeothon@icloud.com>
2017-10-21 01:41:15 +0200

    Added workaround for loading snapsvg

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+  module: {
+    rules: [
+      {
+        test: require.resolve('snapsvg'),
+        use: 'imports-loader?this=>window,fix=>module.exports=0',
+      },
+    ],
+  },
 }
