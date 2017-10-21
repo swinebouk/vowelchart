98bf78584c5320fa509c21fd72f2a82267575d93
Johnny Lee-Othon <jleeothon@icloud.com>
2017-10-21 18:12:37 +0200

    Draw all vowels

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@
 
 import VowelChart from './VowelChart'
 import VowelPlotter from './VowelPlotter'
+import _ from 'lodash'
+import {list} from './VowelList'
 
 window.addEventListener('load', function () {
   const vowelChart = new VowelChart(500)
   const plotter = new VowelPlotter(vowelChart)
   plotter.drawBorder()
+  _.each([...list()], v => plotter.markVowel(v))
 })
 
 document.getElementById('download').addEventListener('click', function () {
