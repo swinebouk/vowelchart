3aa3ee4e6599a2d35b928609e054376bbf3afdab
Leonhardt Wille <leonhardt.wille@erasys.de>
2017-10-24 23:06:16 +0200

    not to get what one wants is suffering;

--- src/views/Chart.js
+++ src/views/Chart.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import './Chart.css'
+// import VowelChart from '../VowelChart'
 
 class Chart extends Component {
   render () {
-    const vowelChart = new VowelChart(500)
-    const plotter = new VowelPlotter(vowelChart)
+    // const vowelChart = new VowelChart(500)
     return (
       <section>
         <svg id="vowel-chart" height="500" width="500"/>
