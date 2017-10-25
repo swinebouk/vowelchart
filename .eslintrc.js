27dac85cd18575c91d09c5e27dafe46a6937db58
Johnny Lee-Othon <jleeothon@icloud.com>
2017-10-26 01:00:07 +0200

    Require eslint-plugin-jsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,13 @@
 module.exports = {
+  "env": {
+    "browser": true,
+    "jest/globals": true,
+  },
   "extends": [
     "standard",
     "plugin:react/recommended",
     "plugin:jest/recommended",
   ],
-  "rules": {
-    "comma-dangle": ["error", "always-multiline"],
-  },
-  "env": {
-    "browser": true,
-    "jest/globals": true,
-  },
   "parserOptions": {
     "ecmaFeatures": {
       "jsx": true
@@ -18,6 +15,26 @@ module.exports = {
   },
   "plugins": [
     "jest",
+    "jsdoc",
     "react",
   ],
+  "rules": {
+    "comma-dangle": ["error", "always-multiline"],
+    "jsdoc/check-param-names": 1,
+    "jsdoc/check-tag-names": 1,
+    "jsdoc/check-types": 1,
+    "jsdoc/newline-after-description": 1,
+    "jsdoc/require-description-complete-sentence": 1,
+    "jsdoc/require-param": 1,
+    "jsdoc/require-param-type": 1,
+    "jsdoc/require-returns-type": 1,
+  },
+  "settings": {
+    "jsdoc": {
+      "tagNamePreference": {
+        "param": "param",
+        "returns": "returns",
+      },
+    },
+  },
 };
