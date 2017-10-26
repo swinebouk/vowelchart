447fd689013118adb59d8126f8d0f1e3e03f72ae
Johnny Lee-Othon <jleeothon@icloud.com>
2017-10-27 00:42:20 +0200

    Parse text such as open+front+unrounded

--- src/VowelChart.test.js
+++ src/VowelChart.test.js
@@ -1,14 +1,37 @@
-import VowelChart from './VowelChart'
 import Vowel from './Vowel'
+import VowelChart from './VowelChart'
+import {
+  Openness as O,
+  Frontness as F,
+  Roundness as R,
+} from './Articulation'
 
-test('calculates the corners correctly', () => {
-  const chart = new VowelChart(100)
-  const topLeft = new Vowel(0, 1, 0)
-  const topRight = new Vowel(0, 0, 0)
-  const bottomLeft = new Vowel(1, 1, 0)
-  const bottomRight = new Vowel(1, 0, 0)
-  expect(chart.vowelXY(topLeft)).toEqual([10, 10])
-  expect(chart.vowelXY(topRight)).toEqual([90, 10])
-  expect(chart.vowelXY(bottomLeft)).toEqual([50, 70])
-  expect(chart.vowelXY(bottomRight)).toEqual([90, 70])
+test('Calculates the primary set positions correctly', () => {
+  const chart = new VowelChart(120)
+  const c1 = new Vowel(O.close, F.front, R.unrounded)
+  const c2 = new Vowel(O.closeMid, F.front, R.unrounded)
+  const c3 = new Vowel(O.openMid, F.front, R.unrounded)
+  const c4 = new Vowel(O.open, F.front, R.unrounded)
+  const c5 = new Vowel(O.open, F.back, R.unrounded)
+  const c6 = new Vowel(O.openMid, F.back, R.unrounded)
+  const c7 = new Vowel(O.closeMid, F.back, R.unrounded)
+  const c8 = new Vowel(O.close, F.back, R.unrounded)
+
+  expect(chart.vowelX(c1)).toBeCloseTo(12)
+  expect(chart.vowelX(c2)).toBeCloseTo(28)
+  expect(chart.vowelX(c3)).toBeCloseTo(44)
+  expect(chart.vowelX(c4)).toBeCloseTo(60)
+  expect(chart.vowelX(c5)).toBeCloseTo(108)
+  expect(chart.vowelX(c6)).toBeCloseTo(108)
+  expect(chart.vowelX(c7)).toBeCloseTo(108)
+  expect(chart.vowelX(c8)).toBeCloseTo(108)
+
+  expect(chart.vowelY(c1)).toBeCloseTo(12)
+  expect(chart.vowelY(c2)).toBeCloseTo(36)
+  expect(chart.vowelY(c3)).toBeCloseTo(60)
+  expect(chart.vowelY(c4)).toBeCloseTo(84)
+  expect(chart.vowelY(c5)).toBeCloseTo(84)
+  expect(chart.vowelY(c6)).toBeCloseTo(60)
+  expect(chart.vowelY(c7)).toBeCloseTo(36)
+  expect(chart.vowelY(c8)).toBeCloseTo(12)
 })
