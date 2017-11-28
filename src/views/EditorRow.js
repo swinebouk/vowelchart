6f5fc52a10f8c3f5a0b0d85681dc1ba269a20dd0
Leonhardt Wille <leonhardt.wille@erasys.de>
2017-11-29 00:24:10 +0100

    refactor: ok this is much better

--- src/views/EditorRow.js
+++ src/views/EditorRow.js
@@ -17,11 +17,27 @@ class EditorRow extends Component {
   static defaultProps= {
     visible: true,
   }
+  updateSymbol (symbol) {
+    this.updateVowel({_symbol: symbol})
+  }
+
+  updateRoundness (roundness) {
+    this.updateVowel({_roundness: roundness === 'true'})
+  }
+
+  updateOpenness (openness) {
+    this.updateVowel({_openness: parseFloat(openness)})
+  }
+
+  updateFrontness (frontness) {
+    this.updateVowel({_frontness: parseFloat(frontness)})
+  }
 
   constructor (props) {
     super(props)
     this.handleChange = this.handleChange.bind(this)
     this.updateVowel = this.updateVowel.bind(this)
+    _.bindAll(this, ['updateFrontness', 'updateOpenness', 'updateRoundness', 'updateSymbol'])
   }
 
   updateVowel (changeset) {
@@ -38,10 +54,10 @@ class EditorRow extends Component {
       <li className="editorRow">
         <label className="checkbox">
           <input type="checkbox" value={this.props.visible} onChange={this.handleChange}/>
-          <SelectArticulation articulation="_frontness" value={this.props._frontness} onChange={this.updateVowel}/>
-          <SelectArticulation articulation="_openness" value={this.props._openness} onChange={this.updateVowel}/>
-          <SelectArticulation articulation="_roundness" value={this.props._rounded} onChange={this.updateVowel}/>
-          <input className="input vowel-name" type="text" placeholder="i" value={this.props._symbol} onChange={this.handleChange} />
+          <SelectArticulation articulation="_frontness" value={this.props._frontness} onChange={this.updateFrontness}/>
+          <SelectArticulation articulation="_openness" value={this.props._openness} onChange={this.updateOpenness}/>
+          <SelectArticulation articulation="_roundness" value={this.props._rounded} onChange={this.updateRoundness}/>
+          <input className="input vowel-name" type="text" placeholder="i" value={this.props._symbol} onChange={this.updateSymbol} />
         </label>
       </li>
     )
