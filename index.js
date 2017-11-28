e111c01f52ab5ed296db634018dfc41a17534f4a
Johnny Lee-Othon <jleeothon@icloud.com>
2017-11-28 23:25:32 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
 const Koa = require('koa')
-const Router = require('koa-oai-router')
-const bodyParser = require('koa-bodyparser')
-
 const app = new Koa()
 
-app.use(bodyParser())
-
-app.use(require('koa-static')('build', {}))
-
-const server = app.listen(process.env.PORT || 3000)
-
-const opt = {
-  apiDoc: './api/api.yaml',
-  controllerDir: './controllers',
-  server: server,
-  versioning: true,
-  apiExplorerVisible: true,
-}
-
-const router = new Router(opt)
-
-app.use(router.routes())
-app.use(router.apiExplorer())
+app.listen(process.env.PORT || 3001)
